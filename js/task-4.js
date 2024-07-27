3dc0f4b7813cd0e965faad84ef5f267bf84ce3cf
versroot <131954932+versroot@users.noreply.github.com>
2024-07-27 22:02:56 +0200

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,16 @@
+const formLog = document.querySelector('.login-form');
 
+formLog.addEventListener('submit', submitFunc);
+
+function submitFunc(event) {
+  event.preventDefault();
+
+  const email = event.target.elements.email.value;
+  const password = event.target.elements.password.value;
+
+  if (email === '' || password === '') {
+    alert('All form fields must be filled in');
+  }
+  console.log(`E-mail: ${email}, Password: ${password}`);
+  formLog.reset();
+}
